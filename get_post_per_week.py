fc56e58460d87844c4406c102065215b61db636f
muhammadkhanusmanov <uqaxromon@gmail.com>
2022-12-01 03:14:41 -0600

    answer

--- get_post_per_week.py
+++ get_post_per_week.py
@@ -1,9 +1,15 @@
 from read_data import fromJson
 def get_post_per_week(data:dict,month:int)->dict:
-    week={
-        0:0,
-        1:0,
-        2:0,
-        3:0
-    }
-    return 
+    a={}
+    for i in range(1,5):
+        k=0
+        for j in data['messages']:
+            if int(j['date'][5:7])==month and j['type']=='message':
+                if (int(j['date'][8:10])>=(7*i-6) and int(j['date'][8:10])<=7*i):
+                    k+=1
+        a[i]=k
+    return a
+
+f=fromJson('data/result.json')
+print(get_post_per_week(f,10))
+
